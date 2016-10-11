5b55105d91a7c306fa9765f184416374f9d6e792
Herro-Sama <sandylily66@hotmail.com>
2016-10-11 15:56:49 +0100

    Paddles Move and stop within the image

--- Pong Code/Pong.py
+++ Pong Code/Pong.py
@@ -3,6 +3,10 @@ from pygame.locals import *
 
 pygame.init()
 
+FPSCLOCK = pygame.time.Clock()
+
+FPS = 4000
+
 Width = 800
 Height = 600
 
@@ -19,10 +23,16 @@ Y2 = 200
 Player1Score = 0
 Player2Score = 0
 
+font = pygame.font.SysFont(None, 96)
 
+Score1 = font.render(Player1Score, True, White, Black)
+
+
+Score2 = font.render(Player2Score, True, White, Black)
 
 while True:
 
+    window.fill(Black)
     Pong1 = pygame.draw.rect(window, White, (50, Y1, 25, 100))
     Pong2 = pygame.draw.rect(window, White, (750, Y2, 25, 100))
 
@@ -34,16 +44,16 @@ while True:
             Y2 = 0
     if keys[K_DOWN]:
         Y2 += 0.25
-        if Y2 == 600:
-            Y2 = 599
+        if Y2 == 500:
+            Y2 = 499
     if keys[K_w]:
         Y1 -= 0.25
-        if Y1 == 600:
-            Y1 = 599
+        if Y1 == -1:
+            Y1 = 0
     if keys[K_s]:
         Y1 += 0.25
-        if Y1 == 600:
-            Y1 = 599
+        if Y1 == 500:
+            Y1 = 499
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -52,3 +62,4 @@ while True:
 
 
     pygame.display.update()
+    FPSCLOCK.tick(FPS)
