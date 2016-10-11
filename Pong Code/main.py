7221b080116f468e4f0ef4ba3e0147537cd01cd7
jack-maber <jack-maber@live.co.uk>
2016-10-11 14:24:10 +0100

    Made window

--- Pong Code/main.py
+++ Pong Code/main.py
@@ -6,9 +6,9 @@ pygame.init()
 Width = 800
 Height = 600
 
-window = pygame.display.set_mode((Width,Height), 0, 32)
+window = pygame.display.set_mode((Width, Height), 0, 32)
 
-pygame.display.set_caption('Backdoor')
+pygame.display.set_caption('Pong')
 
 Black = (0, 0, 0)
 White = (255, 255, 255)
@@ -16,24 +16,10 @@ Red = (255, 0, 0)
 Green = (0, 255, 0)
 Blue = (0, 0, 255)
 
-
-
-font = pygame.font.SysFont(None, 48)
-Backdoor = 1
-
-text = font.render('It works!', True, Green, Blue)
-
-Backdoor_container = text.get_rect()
-Backdoor_container.centerx = window.get_rect().centerx
-Backdoor_container.centery = window.get_rect().centery
-
-
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     window.fill(Black)
-    pygame.draw.rect(window, White, Backdoor_container)
-    window.blit(text, Backdoor_container)
     pygame.display.update()
